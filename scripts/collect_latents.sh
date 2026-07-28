dd78d430ce4accb22c8b865688a86a8ef189a518
kdybek <kam.dyb1@gmail.com>
2026-07-28 14:13:03 +0200

    added run script for latent collection

---
+++ scripts/collect_latents.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --account=plgcredibleai2026-gpu-gh200
+#SBATCH --partition=plgrid-gpu-gh200
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+export XDG_CACHE_HOME=$SCRATCH/.cache
+
+mkdir -p $SCRATCH/terramind_probing
+cd $SCRATCH/terramind_probing
+cp -rf ~/terramind_probing/* .
+
+source .venv/bin/activate
+
+python collect_latents.py
