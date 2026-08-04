51f8e2cb2b857fe8c7a7bad97ffbf1df29f6e100
kdybek <kam.dyb1@gmail.com>
2026-08-04 18:44:30 +0200

    added dataset download script

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=8:00:00
+#SBATCH --account=plgcredibleai2026-cpu
+#SBATCH --partition=plgrid-cpu
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+ml Python/3.11.5
+
+export XDG_CACHE_HOME=$SCRATCH/.cache
+export HF_TOKEN=$(cat ~/.hf_token)
+
+mkdir -p $SCRATCH/terramind_probing
+cd $SCRATCH/terramind_probing
+cp -rf ~/terramind_probing/* .
+
+source .venv/bin/activate
+
+hf auth login
+hf download ibm-esa-geospatial/TerraMesh --repo-type dataset --include "val/*" --local-dir data/TerraMesh
