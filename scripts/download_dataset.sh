09c749059ca75c248b83a18523a63cb0a29c0123
kdybek <kam.dyb1@gmail.com>
2026-08-04 19:15:22 +0200

    bug fix

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -3,9 +3,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
+#SBATCH --gres=gpu:1
 #SBATCH --time=8:00:00
-#SBATCH --account=plgcredibleai2026-cpu
-#SBATCH --partition=plgrid
+#SBATCH --account=plgcredibleai2026-gpu-gh200
+#SBATCH --partition=plgrid-gpu-gh200
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
--- scripts/probe.sh
+++ scripts/probe.sh
@@ -3,9 +3,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128G
+#SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --account=plgcredibleai2026-cpu
-#SBATCH --partition=plgrid
+#SBATCH --account=plgcredibleai2026-gpu-gh200
+#SBATCH --partition=plgrid-gpu-gh200
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
